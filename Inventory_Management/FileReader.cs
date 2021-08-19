80106b6a1f61086e1499efc205f603b57664ecca
Prabal2207 <prabal760@gmail.com>
2021-08-19 22:42:49 +0530

    [Prabal] Added:- Inventory management system

---
+++ Inventory_Management/FileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Inventory_Management
+{
+    public class FileReader
+    {
+        public List<Inventory> inventories = new List<Inventory>();
+       // public Dictionary<string, Inventory> inventories = new Dictionary<string, Inventory>();
+
+        public List<Inventory> GetInventry()
+        {
+            string text = File.ReadAllText("/Users/prabalrai/Projects/Inventory_Management/Inventory_Management/data.json");
+            inventories = JsonConvert.DeserializeObject<List<Inventory>>(text);
+
+
+
+            //int sumTotal = inventories.Sum(inventories => inventories.Price);
+            //Console.WriteLine(sumTotal);
+
+
+            return inventories;
+
+            
+        }
+
+    }
+}
---
+++ Inventory_Management/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Inventory_Management
+{
+   
+        public class Inventory
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+    
+}
