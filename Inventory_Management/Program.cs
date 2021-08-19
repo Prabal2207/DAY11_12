70a96abae2621948fa9d43d83177d4ab2456bf24
Prabal2207 <prabal760@gmail.com>
2021-08-19 22:40:29 +0530

    [Prabal] Added:- This is main file

---
+++ Inventory_Management/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventory_Management
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory Management System");
+            FileReader fileReader = new FileReader();
+            
+           var inventories= fileReader.GetInventry();
+            foreach(var inventory in inventories)
+            {
+                Console.WriteLine($"{ inventory.Name}");
+                Console.WriteLine($"{inventory.Price * inventory.Weight}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
